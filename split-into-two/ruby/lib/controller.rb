81111506b266b34f78168e006b30c10188e0076a
Brian Marick <marick@exampler.com>
2012-02-16 17:15:15 -0600

    Make controller obey desired range

--- split-into-two/ruby/lib/controller.rb
+++ split-into-two/ruby/lib/controller.rb
@@ -3,12 +3,27 @@ class Controller
     @value_tweaker = defaults[:value_tweaker]
     @hardware = defaults[:hardware]
     @authoritative_value = defaults[:starting_value]
+    @allowed_range = defaults[:allowed_range]
   end
 
   def adjust_setting(delta)
-    @authoritative_value += delta
+    @authoritative_value = closest_to_desired_value(delta)
     @value_tweaker.display(@authoritative_value)
     @hardware.update(@authoritative_value)
+  end
+
+  def setting; @authoritative_value; end
+
+  private
+
+  def closest_to_desired_value(delta)
+    desired = @authoritative_value + delta
+    if (desired > @allowed_range.last)
+      desired = @allowed_range.last
+    elsif (desired < @allowed_range.first)
+      desired = @allowed_range.first
+    end
+    desired
   end
 
 end
--- split-into-two/ruby/test/controller-tests.rb
+++ split-into-two/ruby/test/controller-tests.rb
@@ -2,11 +2,15 @@ require "./helper"
 
 class ControllerTest < Test::Unit::TestCase
   DEFAULT_VALUE = 10
+  MIN = 0
+  MAX = 20
+
   def setup
     collaborators :value_tweaker, :hardware
     @sut = Controller.new(value_tweaker: @value_tweaker,
                           hardware: @hardware,
-                          starting_value: DEFAULT_VALUE)
+                          starting_value: DEFAULT_VALUE,
+                          allowed_range: MIN..MAX)
   end
 
   should "change controlled object's displayed values when adjusting" do
@@ -16,5 +20,29 @@ class ControllerTest < Test::Unit::TestCase
       @value_tweaker.receives.display(DEFAULT_VALUE+3)
       @hardware.receives.update(DEFAULT_VALUE+3)
     }
+    assert_equal(DEFAULT_VALUE+3, @sut.setting)
   end
+
+  should "peg values at upper bound of the allowed range" do
+    during {
+      up_to_top = MAX-DEFAULT_VALUE
+      @sut.adjust_setting(up_to_top+1)
+    }.behold! {
+      @value_tweaker.receives.display(MAX)
+      @hardware.receives.update(MAX)
+    }
+    assert_equal(MAX, @sut.setting)
+  end
+
+  should "peg values at lower bound of the allowed range" do
+    during {
+      down_to_bottom = MIN - DEFAULT_VALUE
+      @sut.adjust_setting(down_to_bottom - 1)
+    }.behold! {
+      @value_tweaker.receives.display(MIN)
+      @hardware.receives.update(MIN)
+    }
+    assert_equal(MIN, @sut.setting)
+  end
+
 end
