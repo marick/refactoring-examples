fb4ad8a865f677a9afe40e5582bde16877a0d4ea
Brian Marick <marick@exampler.com>
2012-03-09 17:00:20 -0600

    Reactive code has been split out into its own gem

--- split-into-two/ruby/lib/requires.rb
+++ split-into-two/ruby/lib/requires.rb
@@ -1,5 +1,7 @@
+require "rubactive"
+
+
 require_relative 'util/truly-restrained-monkeypatching'
-require_relative "util/reactive"
 require_relative "hardware"
 require_relative "places"
 require_relative "value-tweaker"
